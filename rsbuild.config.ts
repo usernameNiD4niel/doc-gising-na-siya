e728f35c01b06d1a070084c1962ad13737191dda
Daniel Rey <reyvillanuevadaniel@gmail.com>
2025-03-11 22:03:11 +0800

    added rsbuild output

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["postcss-loader"],
-        type: "css"
-      }
-    ]
-  }
+	plugins: [pluginReact()],
+	output: {
+		assetPrefix: "/doc-gising-na-siya/",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["postcss-loader"],
+				type: "css",
+			},
+		],
+	},
 });
