1ccd1b8c18f294805890029a71376fc4b3167c74
Daniel Rey <reyvillanuevadaniel@gmail.com>
2025-03-12 08:22:31 +0800

    added the zoom effect for the images

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 
 interface Props extends React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> { }
 
 function Image({ ...props }: Props) {
+    const [isZoom, setIsZoom] = useState(false);
+
+    function onClick() {
+        setIsZoom(prev => !prev);
+    }
+
     return (
-        <img {...props} className={`w-44 h-auto md:w-[340px] ${props.className}`} />
+        <div className={isZoom ? "fixed top-0 bottom-0 left-0 right-0 z-10 transition-position duration-150 ease-linear flex items-center justify-center bg-black" : ""} onClick={onClick}>
+            <img {...props} className={`w-44 h-auto md:w-[340px] ${props.className} ${isZoom ? "zoom-in rounded-md" : ""}`} />
+        </div>
     )
 }
 
